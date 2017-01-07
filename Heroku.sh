c95a8a3d0e37b87b982b2991451eb6a1d1edf728
viktorburcev <victor.burtsev@gmail.com>
2017-01-07 16:23:35 +0300

    Deleted parameter -DskipTests=true

--- Heroku.sh
+++ Heroku.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export DATABASE_URL=postgres://postgres:test@localhost:5432/librarymanager
-mvn clean install -DskipTests=true
+mvn clean install
 open -a safari http://localhost:8080
 java -jar target/dependency/webapp-runner.jar target/*.war
