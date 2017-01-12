1b10b41651ee03dde62d44eef234320a43ff0bae
victor <victor.burtsev@gmail.com>
2017-01-12 22:54:20 +0300

    Add '&' in HerokuLocalEmulator.cmd

--- HerokuLocalEmulator.cmd
+++ HerokuLocalEmulator.cmd
@@ -1,3 +1,2 @@
 set DATABASE_URL=postgres://postgres:test@localhost:5432/librarymanager
-mvn clean install
-(ping 127.0.0.1 -n 6 > nul & start http://localhost:8080) | java -jar target/dependency/webapp-runner.jar target/*.war
+mvn clean install & (ping 127.0.0.1 -n 6 > nul & start http://localhost:8080) | java -jar target/dependency/webapp-runner.jar target/*.war
