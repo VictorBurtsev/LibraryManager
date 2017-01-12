e94fed6721ea6b39f35fbb00064937770abc7ef6
viktorburcev <victor.burtsev@gmail.com>
2017-01-12 20:25:29 +0300

    Fix code in HerokuLocalEmulator.cmd (replace sleep 5s command)

--- HerokuLocalEmulator.cmd
+++ HerokuLocalEmulator.cmd
@@ -1,3 +1,3 @@
 set DATABASE_URL=postgres://postgres:test@localhost:5432/librarymanager
 mvn clean install
-sleep 5s & start http://localhost:8080 | java -jar target/dependency/webapp-runner.jar target/*.war
+(ping 127.0.0.1 -n 6 > nul & start http://localhost:8080) | java -jar target/dependency/webapp-runner.jar target/*.war
