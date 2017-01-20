d5cedb063f418583d4b8bb6f46171511b9304119
viktorburcev <victor.burtsev@gmail.com>
2017-01-20 22:09:58 +0300

    Added parameter "--port"

--- HerokuLocalEmulator.sh
+++ HerokuLocalEmulator.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export DATABASE_URL=postgres://postgres:test@localhost:5432/librarymanager
 mvn clean install
-sleep 5 && open http://localhost:8080 &
-java -jar target/dependency/webapp-runner.jar target/*.war
+sleep 5 && open http://localhost:8081 &
+java -jar target/dependency/webapp-runner.jar --port 8081 target/*.war
