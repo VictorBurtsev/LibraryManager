469e757714acc8df284ecf3bec6f1e1252b5625f
viktorburcev <victor.burtsev@gmail.com>
2017-01-22 20:22:18 +0300

    Added parameter "--port 8081"

--- HerokuLocalEmulator.cmd
+++ HerokuLocalEmulator.cmd
@@ -1,2 +1,2 @@
 set DATABASE_URL=postgres://postgres:test@localhost:5432/librarymanager
-mvn clean install & (ping 127.0.0.1 -n 6 > nul & start http://localhost:8080) | java -jar target/dependency/webapp-runner.jar target/*.war
+mvn clean install & (ping 127.0.0.1 -n 6 > nul & start http://localhost:8081) | java -jar target/dependency/webapp-runner.jar --port 8081 target/*.war
