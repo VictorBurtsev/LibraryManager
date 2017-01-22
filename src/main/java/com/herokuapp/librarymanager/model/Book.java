5f4e686ca94b3f32b65ba1e190214ce8bc8be0e8
viktorburcev <victor.burtsev@gmail.com>
2017-01-22 20:44:25 +0300

    Improve Book.java

--- src/main/java/com/herokuapp/librarymanager/model/Book.java
+++ src/main/java/com/herokuapp/librarymanager/model/Book.java
@@ -11,29 +11,9 @@ public class Book {
     @Id
     @SequenceGenerator(name = "books_id_seq", sequenceName = "books_id_seq", allocationSize = 1)
     @GeneratedValue(generator = "books_id_seq", strategy = GenerationType.SEQUENCE)
-    @Column(name = "id")
     private int id;
-
-    @Column(name = "title")
-    private String bookTitle;
-
-    @Column(name = "author")
-    private String bookAuthor;
-
-    @Column(name = "price")
+    private String title;
+    private String author;
     private int price;
-
-    @Column(name = "pages")
     private int pages;
-
-    @Override
-    public String toString() {
-        return "book{" +
-                "id=" + id +
-                ", title='" + bookTitle + '\'' +
-                ", author='" + bookAuthor + '\'' +
-                ", price=" + price + '\'' +
-                ", pages=" + pages +
-                '}';
-    }
 }
